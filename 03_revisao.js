a0d7fe95b3493a941f5e6a6852f7c8ef781e1bfc
Rafael Hiroshi Tejima <129396110+RafaelTejima@users.noreply.github.com>
2026-08-17 14:47:43 -0300

    revisão e acúmulo de variáveis

---
+++ 03_revisao.js
@@ -0,0 +1,31 @@
+let aluguel = 1200.00
+let condominio = 850.59
+let internet = 85.99
+let total = aluguel + condominio + internet
+
+console.log("O total das contas é: " + total)
+// -----------------------------------------------------------------------------
+
+let nome = "Rafael"
+let sobrenome = "Tejima"
+let nome_completo = nome + " " + sobrenome
+console.log("Seja bem-vindo(a) " + nome_completo + "!") 
+
+// -----------------------------------------------------------------------------
+
+let salario = 2000
+console.log("\nSeu salário é: R$" + salario)
+
+let comissão = 100
+salario = salario + comissão
+console.log("Seu salário com comissão é de: R$" + salario) // acumulo: adicionar um novo valor na variável
+
+let adicional_noturno = 200
+salario = salario + adicional_noturno
+console.log("Seu salário com adicional noturno é de: R$" + salario)
+
+/*
+Isso é um comentário 
+de múltiplas linhas
+
+a vontade*/
