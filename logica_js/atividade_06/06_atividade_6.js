08f42c84ec2ab5915f8b5f27d4fa90a79b2a9e2b
Rafael Hiroshi Tejima <129396110+RafaelTejima@users.noreply.github.com>
2026-08-21 15:09:58 -0300

    conclusão da lógica de programação

---
+++ logica_js/atividade_06/06_atividade_6.js
@@ -0,0 +1,11 @@
+// ATIVIDADE 6: Investigação Criminal
+let prompt = require("prompt-sync")()
+let pergunta1 = prompt("Telefonou para a vítima? ")
+let pergunta2 = prompt("Esteve no local do crime? ")
+let pergunta3 = prompt("Mora perto da vítima? ")
+let pergunta4 = prompt("Devia para a vítima? ")
+let pergunta5 = prompt("Já trabalhou com a vítima? ")
+
+if(){
+
+}
